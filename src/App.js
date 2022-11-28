43b9cce769be88aa06dedc9fb0a87512136e061a
Karasova Yuliia <julia.povolotskaya@gmail.com>
2022-11-28 18:28:38 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div>
+      <footer>
+          <div className="github-link pt-4">
+            This project was coded by MartaPatrycja and is{" "}
+            <a
+              href="https://github.com/MartaPatrycja/app-weather-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
+    
   );
 }
 
